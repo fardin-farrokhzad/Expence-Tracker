e46ea76281f51acb4cbb2ecde803815d24a23dc6
Fardin <fardinfarrokhzad@gmail.com>
2025-10-13 17:25:27 +0330

    numberToPersian made efficient

--- src/components/TransanctionTable/TransactionTable.jsx
+++ src/components/TransanctionTable/TransactionTable.jsx
@@ -3,16 +3,7 @@ import styles from './TransactionTable.module.css';
 
 // Converting numbers to persian
 function numberToPersian(number) {
-  const persian = { 0: '۰', 1: '۱', 2: '۲', 3: '۳', 4: '۴', 5: '۵', 6: '۶', 7: '۷', 8: '۸', 9: '۹' };
-  number = number.toString().split('');
-  let persianNumber = '';
-  for (let i = 0; i < number.length; i++) {
-    number[i] = persian[number[i]];
-  }
-  for (let i = 0; i < number.length; i++) {
-    persianNumber += number[i];
-  }
-  return persianNumber;
+  return number.toString().replace(/[0-9]/g, d => '۰۱۲۳۴۵۶۷۸۹'[d]);
 }
 
 // Transaction Table Component
