39abe6f6653fc8cda8a5bb5a1e5141407f689135
Fardin <fardinfarrokhzad@gmail.com>
2025-10-19 13:29:50 +0330

    svg imported as componenet, date convert added, folder name changed

--- src/components/TransactionTable/TransactionTable.jsx
+++ src/components/TransactionTable/TransactionTable.jsx
@@ -3,8 +3,14 @@ import { useState } from 'react';
 import styles from './TransactionTable.module.css';
 import AddTransactionModal from '../AddTransactionModal/AddTransactionModal';
 
+// Import SVG files
+import PlusIcon from '/src/assets/svg/outline/plus.svg?react';
+import DangerCircleIcon from '/src/assets/svg/outline/danger-circle.svg?react';
+import DeleteIcon from '/src/assets/svg/outline/delete.svg?react';
+
 // Convert numbers to Persian
 const numberToPersian = number => number?.toString().replace(/[0-9]/g, d => '۰۱۲۳۴۵۶۷۸۹'[d]) || '';
+const dateConvert = input => input?.toString().replace(/[0-9-]/g, d => (d === '-' ? '/' : '۰۱۲۳۴۵۶۷۸۹'[d])) || '';
 
 // Transaction Table Component
 const TransactionTable = ({ data, addTransaction, removeTransaction }) => {
@@ -21,14 +27,14 @@ const TransactionTable = ({ data, addTransaction, removeTransaction }) => {
         <h2 className={styles.transactions__title}>تراکنش‌ها</h2>
         <button className={styles.transaction__button} onClick={() => setModalOpen(true)}>
           <span>افزودن تراکنش</span>
-          <img src='/src/assets/svg/outline/Plus.svg' alt='' />
+          <PlusIcon className={styles.plus} />
         </button>
       </div>
 
       {/* No Data */}
       {data.length === 0 ? (
         <div className={styles.no__data}>
-          <img src='/src/assets/SVGs/Outline/Danger Circle.svg' alt='' />
+          <DangerCircleIcon className={styles.no__data__icon} />
           <span>شما هنوز تراکنشی وارد نکرده‌اید</span>
         </div>
       ) : (
@@ -47,7 +53,7 @@ const TransactionTable = ({ data, addTransaction, removeTransaction }) => {
               <div className={styles.transaction} key={item.id}>
                 {/* Date */}
                 <div className={`${styles.transaction__item} ${styles.date}`}>
-                  <span>{numberToPersian(item.date)}</span>
+                  <span>{dateConvert(item.date)}</span>
                 </div>
 
                 {/* Income */}
@@ -72,7 +78,7 @@ const TransactionTable = ({ data, addTransaction, removeTransaction }) => {
                 {/* Delete button */}
                 <div className={styles.transaction__item}>
                   <button className={styles.deleteButton} onClick={() => removeTransaction(item.id)}>
-                    <img src='/src/assets/svg/outline/Delete.svg' className={styles.delete} alt='' />
+                    <DeleteIcon className={styles.delete} />
                   </button>
                 </div>
               </div>
