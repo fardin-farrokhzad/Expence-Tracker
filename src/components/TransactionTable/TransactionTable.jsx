fad6933021b1c5ec13843050feec8cb5b12a4796
Fardin <fardinfarrokhzad@gmail.com>
2025-10-22 20:01:40 +0330

    split TransactionList into a separate component , replace arrow functions with function declarations

--- src/components/TransactionTable/TransactionTable.jsx
+++ src/components/TransactionTable/TransactionTable.jsx
@@ -1,23 +1,16 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import styles from './TransactionTable.module.css';
 import AddTransactionModal from '../AddTransactionModal/AddTransactionModal';
-
-// Import SVG files
 import PlusIcon from '../../assets/svg/outline/plus.svg?react';
 import DangerCircleIcon from '../../assets/svg/outline/danger-circle.svg?react';
-import DeleteIcon from '../../assets/svg/outline/delete.svg?react';
+import TransactionList from '../TransactionList/TransactionList';
 
-// Convert numbers to Persian
-const numberToPersian = number => number?.toString().replace(/[0-9]/g, d => '۰۱۲۳۴۵۶۷۸۹'[d]) || '';
-const dateConvert = input => input?.toString().replace(/[0-9-]/g, d => (d === '-' ? '/' : '۰۱۲۳۴۵۶۷۸۹'[d])) || '';
-
-// Transaction Table Component
-const TransactionTable = ({ data, addTransaction, removeTransaction }) => {
+function TransactionTable({ data, addTransaction, removeTransaction }) {
   const [modalOpen, setModalOpen] = useState(false);
 
-  const handleAddTransaction = transaction => {
+  function handleAddTransaction(transaction) {
     addTransaction(transaction);
-  };
+  }
 
   return (
     <section className={styles.transactions}>
@@ -37,58 +30,12 @@ const TransactionTable = ({ data, addTransaction, removeTransaction }) => {
           <span>شما هنوز تراکنشی وارد نکرده‌اید</span>
         </div>
       ) : (
-        <>
-          {/* Table Header */}
-          <div className={`${styles.transactions__header} ${styles.desktop__only}`}>
-            <span>تاریخ</span>
-            <span>درآمد (تومان)</span>
-            <span>هزینه (تومان)</span>
-            <span>شرح</span>
-          </div>
-
-          {/* Transactions */}
-          <div className={styles.transactions__list}>
-            {data.map(item => (
-              <div className={styles.transaction} key={item.id}>
-                {/* Date */}
-                <div className={`${styles.transaction__item} ${styles.date}`}>
-                  <span>{dateConvert(item.date)}</span>
-                </div>
-
-                {/* Income */}
-                <div className={styles.transaction__item}>
-                  <span className={`${styles.income} ${item.type === 'income' && item.amount > 0 ? styles.hasValue : ''}`}>
-                    {item.type === 'income' && item.amount > 0 ? `${numberToPersian(item.amount)}+` : ''}
-                  </span>
-                </div>
-
-                {/* Expense */}
-                <div className={styles.transaction__item}>
-                  <span className={`${styles.expense} ${item.type === 'expense' && item.amount > 0 ? styles.hasValue : ''}`}>
-                    {item.type === 'expense' && item.amount > 0 ? `${numberToPersian(item.amount)}-` : ''}
-                  </span>
-                </div>
-
-                {/* Description */}
-                <div className={`${styles.transaction__item} ${styles.description}`}>
-                  <span>{item.description}</span>
-                </div>
-
-                {/* Delete button */}
-                <div className={styles.transaction__item}>
-                  <button className={styles.deleteButton} onClick={() => removeTransaction(item.id)}>
-                    <DeleteIcon className={styles.delete} />
-                  </button>
-                </div>
-              </div>
-            ))}
-          </div>
-        </>
+        <TransactionList data={data} removeTransaction={removeTransaction} />
       )}
 
       <AddTransactionModal isOpen={modalOpen} onClose={() => setModalOpen(false)} onSubmit={handleAddTransaction} />
     </section>
   );
-};
+}
 
 export default TransactionTable;
