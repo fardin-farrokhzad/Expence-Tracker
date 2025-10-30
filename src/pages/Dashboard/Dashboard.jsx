50b274631455f129002b252023a36d57db559f0d
Fardin <fardinfarrokhzad@gmail.com>
2025-10-30 13:02:48 +0330

    dashboard component added

---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Dashboard.module.css';
+
+function Dashboard() {
+  return <div className={styles.container}>داشبورد</div>;
+}
+
+export default Dashboard;
