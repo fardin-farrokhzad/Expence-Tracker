186c77d8b57888a31f9712f002ba0f9aa4ed9454
Fardin <fardinfarrokhzad@gmail.com>
2025-10-30 13:03:21 +0330

    404 page added

---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './NotFound.module.css';
+import Message from '/src/assets/svg/outline/notFound.svg?react';
+
+function NotFound() {
+  return (
+    <div className={styles.container}>
+      <Message className={styles.message} />
+      <span className={styles.text}> ! صفحه مورد نظر پیدا نشد</span>
+    </div>
+  );
+}
+
+export default NotFound;
