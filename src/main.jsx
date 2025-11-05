72983bcd5aabb7d14819f098cfe026643b4d0eb8
Fardin <fardinfarrokhzad@gmail.com>
2025-11-05 12:24:37 +0330

    wrap app with TransactionProvider to enable global state access

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
+import { TransactionProvider } from './context/TransactionContext.jsx';
 import './index.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <TransactionProvider>
+        <App />
+      </TransactionProvider>
     </BrowserRouter>
   </StrictMode>
 );
